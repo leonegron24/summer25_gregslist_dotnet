c4f2c2064a1fd8089d661b5bfc09e78165e000b8
Jeremy Fowler <jembyfowler@gmail.com>
2025-07-30 09:51:03 -0600

    cars table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,60 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name VARCHAR(255) COMMENT 'User Name',
-  email VARCHAR(255) UNIQUE COMMENT 'User Email',
-  picture VARCHAR(255) COMMENT 'User Picture'
-) default charset utf8mb4 COMMENT '';
+CREATE TABLE
+  IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL PRIMARY KEY COMMENT 'primary key',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name VARCHAR(255) COMMENT 'User Name',
+    email VARCHAR(255) UNIQUE COMMENT 'User Email',
+    picture VARCHAR(255) COMMENT 'User Picture'
+  ) DEFAULT charset utf8mb4 COMMENT '';
+
+CREATE TABLE
+  cars (
+    -- id should always be your first column
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    make VARCHAR(50) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    `year` SMALLINT UNSIGNED NOT NULL,
+    price MEDIUMINT UNSIGNED NOT NULL,
+    img_url VARCHAR(500) NOT NULL,
+    description VARCHAR(500),
+    engine_type ENUM ('V6', 'V8', 'V10', '4-cylinder', 'unknown') NOT NULL,
+    color VARCHAR(50) NOT NULL,
+    mileage MEDIUMINT UNSIGNED NOT NULL,
+    has_clean_title BOOLEAN DEFAULT TRUE NOT NULL,
+    creator_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES accounts (id) ON DELETE CASCADE
+  );
+
+DROP TABLE cars;
+
+INSERT INTO
+  cars (
+    make,
+    model,
+    `year`,
+    price,
+    img_url,
+    description,
+    engine_type,
+    color,
+    mileage,
+    has_clean_title,
+    creator_id
+  )
+VALUES
+  (
+    'vw',
+    'golf',
+    1980,
+    40000,
+    'https://images.unsplash.com/photo-1751528962027-ac9f0370ff5d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8',
+    'guacomobile',
+    'V8',
+    'avocado',
+    100000,
+    FALSE,
+    '65f87bc1e02f1ee243874743'
+  );
