aa19cba9efb568c60d7068058d26496488a5af1c
Jeremy Fowler <jembyfowler@gmail.com>
2025-07-30 09:20:59 -0600

    filled out env and log in

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+// TODO use your own auth stuff
+export const domain = 'dev-h63x8ohlbl1q2qkp.us.auth0.com'
+export const clientId = 'XX15k7a9Be1KE1Usl1aaOrDdzKJwvtUp'
+export const audience = 'https://jeremyisaraddude.com'
